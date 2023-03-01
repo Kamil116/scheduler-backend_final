8f5407609292f4c89907280fc684cd89de2d0865
Daniel Atonge <elamboatonge@gmail.com>
2023-03-01 23:38:51 +0300

    updated telegram token

--- app/telegram/core/data.py
+++ app/telegram/core/data.py
@@ -2,7 +2,7 @@ MODULE_NAME = "core"
 
 # TOKEN = "625620708:AAFII7HO_Anlcn0pLMUN9Y_HNkTDJ0MLctc"  # - worksilently_bot
 # TOKEN = "6057962313:AAEEDkC3cDA0j3JQBsqr6FlMorSPzXQ7uvs"  # - test_inno_schedule_bot
-TOKEN = "1360816203:AAGlHpdXrWlv6p7EGEImAgRTqQOoUNMkbC0"  # - inno_schedule_bot
+TOKEN = "1360816203:AAFGwIslhxZJmYIQsjAa2GxX1COhJ21vnG4"  # - inno_schedule_bot
 
 MESSAGE_HI = "Hi there!✋"
 MESSAGE_HELP = "Schedule platform for Innopolis students.\n\n" \
